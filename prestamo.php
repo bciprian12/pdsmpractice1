56f60c3376a28c48675dd4797cb4539fb0cf1ed3
Byron Ciprian <bciprian12@gmail.com>
2020-07-17 06:48:56 -0400

    add function to process time and others changes

--- prestamo.php
+++ prestamo.php
@@ -1,39 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Calculadora de pretamo</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+    <form action="prestamo.php" method="post">
+
+        <label for="v_inmueble">Valor del inmueble</label>
+        <input type="number" id="v_inmueble" name="v_inmueble" required autofocus>
+        <br><br>
+        <label for="p_inicial">Pago inicial</label>
+        <input type="number" id="p_inicial" name="p_inicial" required>
+        <br><br>
+        <label for="interes">taza de Interes anual (%)</label>
+        <input type="number" id="interes" name="interes" required>
+        <br><br>
+        <label for="periodo">Periodo de Pago (meses)</label>
+        <input type="number" id="periodo" name="periodo" required>
+        <br><br>
+        <button type="submit">Calcular</button>
+    </form>
+
+
+</body>
+</html>
+
+
 <?php
+
+
 $v_inmueble = $_POST['v_inmueble'];
 $p_inicial = $_POST['p_inicial'];
 $interes = $_POST['interes'];
 $periodo = $_POST['periodo'];
 
 $capital = $v_inmueble - $p_inicial;
-
 $interes = $interes /100;
-
 $pago_mensual = $interes / 12 * pow( 1+ $interes / 12, $periodo) / (pow(1 + $interes/12, $periodo) -1) * $capital;
-
 $total=round($pago_mensual * $periodo,2);
 
-if ($periodo < 12)
+function tiempo($tiempo)
 {
-$meses = $periodo;
-$tiempo = $meses. ' meses';
+    if ($tiempo <= 12)
+    {
+       return $tiempo = $tiempo . ' meses';
+    }
+    elseif($tiempo % 12 == 0 && $tiempo > 12)
+    {
+      return  $tiempo = $tiempo/12 . ' años';
+    }
+    else
+    {
+       return $tiempo = intdiv($tiempo,12) . ' años y ' . $tiempo % 12 . ' meses';
+    }
 }
-elseif ($periodo % 12 == 0)
-{
-$anos = $periodo / 12;
-$tiempo = $anos . ' años ';
-} 
-else
-{
-//intdiv para que solo me de el resultado entero de la division
-$anos = intdiv($periodo,12);
-$meses = $periodo % 12;
-$tiempo = $anos . ' años y ' . $meses. ' meses';
-}
+
+
 
 
 echo 'El pago mensual seria de US$' . round($pago_mensual,2) .'<br>';
 
-echo "Para una hipoteca de US$$capital amortizado en $tiempo <br>";
+echo "Para una hipoteca de US$$capital amortizado en " . tiempo($periodo) . '<br>';
 
 echo 'Hipoteca total con interes US$' . $total ;
 
